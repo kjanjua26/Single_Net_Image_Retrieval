251b81a3e7b8d27b7f6fd1c49258f664d0b2679f
Kamran Janjua <kamranejaz98@gmail.com>
2018-06-21 06:48:20 +0300

    Update singleNet.py

--- singleNet.py
+++ singleNet.py
@@ -5,14 +5,16 @@ import pandas as pd
 import math
 import extract_features
 import nltk
+import network
 # Using the flickr30k dataset 
 
 sentences_fileName = ''
 images_fileName = ''
+num_steps = 500
+steps_per_epoch = 100
+restore_path = 'models/'
 
-correct_X = tf.placeholder(tf.float32, [None, 2])
-wrong_X = tf.placeholder(tf.float32, [None, 2])
-
+# temporary data 
 sample_sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
 			['this', 'is', 'the', 'second', 'sentence'],
 			['yet', 'another', 'sentence'],
@@ -47,3 +49,37 @@ print("2D Image Shape: ", img_features_2d.shape)
 
 correct_fts = np.concatenate((img_features_2d, sent_features_2d), axis=0)
 print("Total Shape: ", correct_fts.shape)
+
+
+# train the network 
+def train():
+	correct_X = tf.placeholder(tf.float32, [None, 2])
+	wrong_X = tf.placeholder(tf.float32, [None, 2])
+
+	loss = network.compute_loss(correct_X, wrong_X)
+	global_step = tf.Variable(0, trainable=False)
+    init_learning_rate = 0.0001
+    learning_rate = tf.train.exponential_decay(init_learning_rate, global_step, steps_per_epoch, 0.794, staircase=True)
+    optim = tf.train.AdamOptimizer(init_learning_rate)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        train_step = optim.minimize(loss, global_step=global_step)
+    saver = tf.train.Saver(save_relative_paths=True)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        if restore_path:
+            print('restoring checkpoint', restore_path)
+            saver.restore(sess, restore_path.replace('.meta', ''))
+            print('done')
+        for i in range(num_steps):
+        	 feed_dict = {
+                    correct_X: correct_fts,
+                    wrong_X : wrong_fts
+              	}
+        	_, loss_Val = sess.run([train_step, loss], feed_dict=feed_dict)
+        	if i % 10 == 0: 
+        		print('Epoch: %d Step: %d Loss: %f' % (i // steps_per_epoch, i, loss_Val))
+    		if i % steps_per_epoch == 0 and i > 0:
+	            print('Saving checkpoint at step %d' % i)
+	            saver.save(sess, FLAGS.save_dir, global_step = global_step)
