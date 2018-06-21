4958300a5f01d9f3533689ee63981498d7166a1a
Kamran Janjua <kamranejaz98@gmail.com>
2018-06-21 07:08:06 +0300

    Update singleNet.py

--- singleNet.py
+++ singleNet.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd 
 import math
 import extract_features
-import nltk
 import network
 # Using the flickr30k dataset 
 
@@ -12,10 +11,11 @@ sentences_fileName = ''
 images_fileName = ''
 num_steps = 500
 steps_per_epoch = 100
+init_learning_rate = 0.0001
 restore_path = 'models/'
 
-correct_X = tf.placeholder(tf.float32, [None, 2])
-wrong_X = tf.placeholder(tf.float32, [None, 2])
+correct_X = tf.placeholder(tf.float32, shape=[None, 2])
+wrong_X = tf.placeholder(tf.float32, shape=[None, 2])
 
 # temporary data 
 sample_sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
@@ -29,28 +29,24 @@ img_dec = [['A person wearing a black jacket is bending over a table'],
 		['An old person in black jacket is trying to pick up something']]
 img = 'sampleImages/1.jpg'
 
-def tokenize_sentences(img_desc):
-	tokens = []
-	for i in img_desc:
-		for j in i:
-			tokens.append(nltk.word_tokenize(j))
-	return tokens 
-
-tokens = tokenize_sentences(img_dec)
+tokens = extract_features.tokenize_sentences(img_dec)
 
 img_features = extract_features.get_img_features_vgg16(img)
 sent_features = extract_features.get_w2v_model(tokens)
+sent_fts_wr = extract_features.get_w2v_model(sample_sentences)
 
 print("Reshaping the features!")
 print("Image Shape: ", img_features.shape)
 print("Sentence Shape: ", sent_features.shape)
 img_features_2d = np.reshape(img_features, (-1, 2)) # converting to 2d feature array
 sent_features_2d = np.reshape(sent_features, (-1, 2))
+sent_ft_wr_2d = np.reshape(sent_fts_wr, (-1, 2))
 print("Img size: ", img_features.size)
 print("sent_features_2d shape: ", sent_features_2d.shape)
 print("2D Image Shape: ", img_features_2d.shape)
-
+print("sent_ft_wr_2d shape: ",sent_ft_wr_2d.shape)
 correct_fts = np.concatenate((img_features_2d, sent_features_2d), axis=0)
+wrong_fts = np.concatenate((img_features_2d, sent_ft_wr_2d), axis=0)
 print("Total Shape: ", correct_fts.shape)
 
 
@@ -58,7 +54,6 @@ print("Total Shape: ", correct_fts.shape)
 def train():
 	loss = network.compute_loss(correct_X, wrong_X)
 	global_step = tf.Variable(0, trainable=False)
-	init_learning_rate = 0.0001
 	learning_rate = tf.train.exponential_decay(init_learning_rate, global_step, steps_per_epoch, 0.794, staircase=True)
 	optim = tf.train.AdamOptimizer(init_learning_rate)
 	update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -75,7 +70,7 @@ def train():
 		for i in range(num_steps):
 			feed_dict = {
 			    correct_X: correct_fts,
-			    wrong_X: correct_fts
+			    wrong_X: wrong_fts
 			}
 			_, loss_Val = sess.run([train_step, loss], feed_dict=feed_dict)
 			if i % 10 == 0: 
