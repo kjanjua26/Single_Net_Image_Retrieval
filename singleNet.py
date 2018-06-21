857be875624bb13b44647519fbc85d162c38b5a0
Kamran Janjua <kamranejaz98@gmail.com>
2018-06-21 04:50:38 +0300

    Update singleNet.py

--- singleNet.py
+++ singleNet.py
@@ -13,13 +13,13 @@ import extract_features
 sentences_fileName = ''
 images_fileName = ''
 
-sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
+sample_sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
 			['this', 'is', 'the', 'second', 'sentence'],
 			['yet', 'another', 'sentence'],
 			['one', 'more', 'sentence'],
 			['and', 'the', 'final', 'sentence']]
 
-sentence_embedding, _ = extract_features.get_glove_model(sentences) # returns words not in vocab as well. 
-w2v_emb = extract_features.get_w2v_model(sentences)
-print("This", w2v_emb[0])
-print("This", sentence_embedding[0])
+sentence_embedding, _ = extract_features.get_glove_model(sample_sentences) # returns words not in vocab as well. 
+w2v_emb = extract_features.get_w2v_model(sample_sentences)
+print("this", w2v_emb[0])
+print("this", sentence_embedding[0])
