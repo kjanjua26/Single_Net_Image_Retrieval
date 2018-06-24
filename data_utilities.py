1ef24adfe5959f2b431bd798c134715e2b0b6a77
Kamran Janjua <kamranejaz98@gmail.com>
2018-06-24 06:13:39 +0300

    Pushed dataset preprocessing code

---
+++ data_utilities.py
@@ -0,0 +1,30 @@
+import tensorflow as tf 
+import numpy as np 
+import pandas as pd 
+import math, glob
+import extract_features
+
+sent_path = 'flicker-sample/sentences-flicker.txt' # a text file containing sentences
+img_path = 'flicker-sample/images'
+sent_file = open(sent_path, 'r').readlines()
+
+def _get_sentences(img_name):	
+	for i in sent_file:
+		if i.find(img_name) == -1:
+			pass
+		else:
+			return ' '.join(i.split()[1:len(i.split())-1]) 
+
+for i in glob.glob(img_path+"/*.jpg"):
+	sent_ft_lst = []
+	i = i.split("/", 2)
+	name = i[2].replace(".jpg","")
+	img_name = "flicker-sample/images/" + i[2]
+	img_ft = extract_features.get_img_features_vgg16(img_name)
+	print "For {0}".format(img_name)
+	sents_rt = _get_sentences(name)
+	sent_fts = extract_features.get_doc2v_model(sents_rt)
+	print "Feature: ", type(sent_fts)
+	sent_ft_lst.append(sent_fts)
+	print "Features: ", type(img_ft)
+	print ""
