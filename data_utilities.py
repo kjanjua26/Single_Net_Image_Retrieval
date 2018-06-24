38e702b31285e876d141b9df63012af2bc0aa888
Kamran Janjua <kamranejaz98@gmail.com>
2018-06-24 07:09:10 +0300

    Update data_utilities.py

--- data_utilities.py
+++ data_utilities.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np 
 import pandas as pd 
 import math, glob
+from gensim.models.doc2vec import TaggedDocument
 import extract_features
 
 sent_path = 'flicker-sample/sentences-flicker.txt' # a text file containing sentences
@@ -9,22 +10,26 @@ img_path = 'flicker-sample/images'
 sent_file = open(sent_path, 'r').readlines()
 
 def _get_sentences(img_name):	
+	img_desc = []
 	for i in sent_file:
 		if i.find(img_name) == -1:
 			pass
 		else:
-			return ' '.join(i.split()[1:len(i.split())-1]) 
+			sent = ' '.join(i.split()[1:len(i.split())-1]) 
+			img_desc.append(TaggedDocument(words=sent.split(), tags=[img_name]))
+	return img_desc
 
-for i in glob.glob(img_path+"/*.jpg"):
-	sent_ft_lst = []
-	i = i.split("/", 2)
-	name = i[2].replace(".jpg","")
-	img_name = "flicker-sample/images/" + i[2]
-	img_ft = extract_features.get_img_features_vgg16(img_name)
-	print "For {0}".format(img_name)
-	sents_rt = _get_sentences(name)
-	sent_fts = extract_features.get_doc2v_model(sents_rt) # doesn't work properly.
-	print "Feature: ", type(sent_fts)
-	sent_ft_lst.append(sent_fts)
-	print "Features: ", type(img_ft)
-	print ""
+def _get_sent_img_features():
+	for i in glob.glob(img_path+"/*.jpg"):
+		i = i.split("/", 2)
+		name = i[2].replace(".jpg","")
+		img_name = "flicker-sample/images/" + i[2]
+		img_ft = extract_features.get_img_features_vgg16(img_name)
+		print "For {0}".format(img_name)
+		sent_list = _get_sentences(name)
+		sent_fts = extract_features.get_doc2v_model(sent_list)
+		print "Sentence Feature: ", type(sent_fts), sent_fts.shape
+		print "Image Features: ", type(img_ft), img_ft.shape
+		print ""
+
+_get_sent_img_features()
