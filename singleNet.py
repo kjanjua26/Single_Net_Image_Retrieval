a57217006bcbb929180b40897c55efde569c7661
Kamran Janjua <kamranejaz98@gmail.com>
2018-07-04 12:33:29 +0200

    Update singleNet.py

--- singleNet.py
+++ singleNet.py
@@ -2,7 +2,7 @@ from __future__ import print_function, absolute_import, division # the handle th
 import tensorflow as tf 
 import numpy as np 
 import pandas as pd 
-import math
+import math, os
 import extract_features
 import network
 import data_utilities
@@ -11,47 +11,15 @@ import data_utilities
 sentences_fileName = ''
 images_fileName = ''
 num_steps = 500
+BATCH_SIZE = 2
 steps_per_epoch = 100
 init_learning_rate = 0.0001
-num_train_samples = 5
+num_train_samples = 31783
 restore_path = 'models/'
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 correct_X = tf.placeholder(tf.float32, shape=[None, 2])
 wrong_X = tf.placeholder(tf.float32, shape=[None, 2])
-
-"""
-	# temporary data 
-	sample_sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
-				['this', 'is', 'the', 'second', 'sentence'],
-				['yet', 'another', 'sentence'],
-				['one', 'more', 'sentence'],
-				['and', 'the', 'final', 'sentence']]
-
-	img_dec = [['A person wearing a black jacket is bending over a table'],
-			['An old person is wearing a black jacket'], 
-			['An old person in black jacket is trying to pick up something']]
-	img = 'sampleImages/1.jpg'
-
-	tokens = extract_features.tokenize_sentences(img_dec)
-
-	img_features = extract_features.get_img_features_vgg16(img)
-	sent_features = extract_features.get_w2v_model(tokens)
-	sent_fts_wr = extract_features.get_w2v_model(sample_sentences)
-
-	print("Reshaping the features!")
-	print("Image Shape: ", img_features.shape)
-	print("Sentence Shape: ", sent_features.shape)
-	img_features_2d = np.reshape(img_features, (-1, 2)) # converting to 2d feature array
-	sent_features_2d = np.reshape(sent_features, (-1, 2))
-	sent_ft_wr_2d = np.reshape(sent_fts_wr, (-1, 2))
-	print("Img size: ", img_features.size)
-	print("sent_features_2d shape: ", sent_features_2d.shape)
-	print("2D Image Shape: ", img_features_2d.shape)
-	print("sent_ft_wr_2d shape: ",sent_ft_wr_2d.shape)
-	correct_fts = np.concatenate((img_features_2d, sent_features_2d), axis=0)
-	wrong_fts = np.concatenate((img_features_2d, sent_ft_wr_2d), axis=0)
-	print("Total Shape: ", correct_fts.shape)
-"""
 
 corr_fts_train = data_utilities._get_training_data_corr()
 wrong_fts_train = data_utilities._get_training_data_wrong()
@@ -70,15 +38,16 @@ def train():
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		for i in range(num_steps):
-			feed_dict = {
-			    correct_X: corr_fts_train,
-			    wrong_X: wrong_fts_train
-			}
-			_, loss_Val = sess.run([train_step, loss], feed_dict=feed_dict)
-			print('Epoch: %d Step: %d Loss: %f' % (i // steps_per_epoch, i, loss_Val))
-			if i % steps_per_epoch == 0 and i > 0:
-			    print('Saving checkpoint at step %d' % i)
-			    #saver.save(sess, FLAGS.save_dir, global_step = global_step)
+			for start, end in zip(range(0, num_train_samples, BATCH_SIZE),range(BATCH_SIZE, num_train_samples + 1,BATCH_SIZE)): # batches
+				feed_dict = {
+				    correct_X: corr_fts_train[start:end],
+				    wrong_X: wrong_fts_train[start:end]
+				}
+				_, loss_Val = sess.run([train_step, loss], feed_dict=feed_dict)
+				print('Epoch: %d Step: %d Loss: %f' % (i // steps_per_epoch, i, loss_Val))
+				if i % steps_per_epoch == 0 and i > 0:
+				    print('Saving checkpoint at step %d' % i)
+				    #saver.save(sess, FLAGS.save_dir, global_step = global_step)
 
 
 if __name__ == '__main__':
