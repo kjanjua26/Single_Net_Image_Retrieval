c98d3202cdc9159f88fdc553bb67ed0b47758336
Kamran Janjua <kamranejaz98@gmail.com>
2018-07-05 11:34:35 +0200

    Update network.py

--- Supervised_Model/network.py
+++ Supervised_Model/network.py
@@ -1,39 +1,53 @@
 import tensorflow as tf 
+from tensorflow.contrib.layers.python.layers import fully_connected
 import numpy as np 
 
-def pairwise_loss(x1, x2):
-	# reshaping the tensors 
-	x1_reshaped = tf.reshape(tf.reduce_sum(x1*x1, axis=1), [-1, 1])
-    	x2_reshaped = tf.reshape(tf.reduce_sum(x2*x2, axis=1), [1, -1])
-	two_point_dist = tf.sqrt(x1_reshaped - 2 * tf.matmul(x1, tf.transpose(x2)) + x2_reshaped + 1e-4) # computing the two point distance. 
-	#l2diff = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(x1, x2)), reduction_indices=1))
-	margin = tf.constant(1.)     
-	match_loss = tf.square(two_point_dist, 'match_term')
-	mismatch_loss = tf.maximum(0., tf.subtract(margin, tf.square(two_point_dist)), 'mismatch_term')
-	loss = tf.add(1., tf.multiply(1., mismatch_loss), 'loss_add')
-	loss_mean = tf.reduce_mean(loss)
-	return loss_mean
+def add_fc(inputs, outdim, train_phase, scope_in):
+    fc =  fully_connected(inputs, outdim, activation_fn=None, scope=scope_in + '/fc')
+    fc_bnorm = tf.layers.batch_normalization(fc, momentum=0.1, epsilon=1e-5,
+                         training=train_phase, name=scope_in + '/bnorm')
+    fc_relu = tf.nn.relu(fc_bnorm, name=scope_in + '/relu')
+    fc_out = tf.layers.dropout(fc_relu, seed=0, training=train_phase, name=scope_in + '/dropout')
+    return fc_out
 
-def network(corr_X, neg_X):
-	# for corr_X
-	corr_fconn_1 = tf.contrib.layers.fully_connected(corr_X, 2048, activation_fn=None, scope='corr_fconn_1')
-	corr_fconn_1_batch_norm = tf.layers.batch_normalization(corr_fconn_1, momentum=0.1, epsilon=1e-5, training=True, name='corr_fconn_1_batch_norm')
-	corr_fconn_1_relu = tf.nn.relu(corr_fconn_1_batch_norm, name='corr_fconn_1_relu')
-	corr_fconn_1_dropout = tf.layers.dropout(corr_fconn_1_relu, seed=0, training=True, name='corr_fconn_1_dropout')
-	corr_fconn_2 = tf.contrib.layers.fully_connected(corr_fconn_1_dropout, 512, activation_fn=None, scope='corr_fconn_2')
-	corr_X_emb = tf.nn.l2_normalize(corr_fconn_2, 1, epsilon=1e-10)
+def pdist(x1, x2):
+    x1_square = tf.reshape(tf.reduce_sum(x1*x1, axis=1), [-1, 1])
+    x2_square = tf.reshape(tf.reduce_sum(x2*x2, axis=1), [1, -1])
+    return tf.sqrt(x1_square - 2 * tf.matmul(x1, tf.transpose(x2)) + x2_square + 1e-4)
 
-	# for neg_X
-	neg_fconn_1 = tf.contrib.layers.fully_connected(neg_X, 2048, activation_fn=None, scope='neg_fconn_1')
-	neg_fconn_1_batch_norm = tf.layers.batch_normalization(neg_fconn_1, momentum=0.1, epsilon=1e-5, training=True, name='neg_fconn_1_batch_norm')
-	neg_fconn_1_relu = tf.nn.relu(neg_fconn_1_batch_norm, name='neg_fconn_1_relu')
-	neg_fconn_1_dropout = tf.layers.dropout(neg_fconn_1_relu, seed=0, training=True, name='neg_fconn_1_dropout')
-	neg_fconn_2 = tf.contrib.layers.fully_connected(neg_fconn_1_dropout, 512, activation_fn=None, scope='neg_fconn_2')
-	neg_X_emb = tf.nn.l2_normalize(neg_fconn_2, 1, epsilon=1e-10)
+def get_network(img_emb, sent_emb):
+	im_fc1 = add_fc(img_emb, 2048, True, 'im_embed_1')
+	im_fc2 = fully_connected(im_fc1, 512, activation_fn=None, scope = 'im_embed_2')
+	i_embed = tf.nn.l2_normalize(im_fc2, 1, epsilon=1e-10)
+	sent_fc1 = add_fc(sent_emb, 2048, True,'sent_embed_1')
+	sent_fc2 = fully_connected(sent_fc1, 512, activation_fn=None, scope = 'sent_embed_2')
+	s_embed = tf.nn.l2_normalize(sent_fc2, 1, epsilon=1e-10)
+	return i_embed, s_embed
 
-	return corr_X_emb, neg_X_emb
+def embedding_loss(im_embeds, sent_embeds, im_labels):
+    sent_im_ratio = 2 # Sample_size 
+    num_img = 40 # Batch_Size
+    num_sent = num_img * sent_im_ratio
+    im_loss_factor = 1.5
+    margin = 0.05
+    sent_only_loss_factor=0.05
+    sent_im_dist = pdist(sent_embeds, im_embeds)
+    pos_pair_dist = tf.reshape(tf.boolean_mask(sent_im_dist, im_labels), [num_sent, 1])
+    neg_pair_dist = tf.reshape(tf.boolean_mask(sent_im_dist, ~im_labels), [num_sent, -1])
+    im_loss = tf.clip_by_value(margin+ pos_pair_dist - neg_pair_dist, 0, 1e6)
+    im_loss = tf.reduce_mean(tf.nn.top_k(im_loss, k=4)[0])
+    neg_pair_dist = tf.reshape(tf.boolean_mask(tf.transpose(sent_im_dist), ~tf.transpose(im_labels)), [num_img, -1])
+    neg_pair_dist = tf.reshape(tf.tile(neg_pair_dist, [1, sent_im_ratio]), [num_sent, -1])
+    sent_loss = tf.clip_by_value(margin + pos_pair_dist - neg_pair_dist, 0, 1e6)
+    sent_loss = tf.reduce_mean(tf.nn.top_k(sent_loss, k=4)[0])
+    sent_sent_dist = pdist(sent_embeds, sent_embeds)
+    sent_sent_mask = tf.reshape(tf.tile(tf.transpose(im_labels), [1, sent_im_ratio]), [num_sent, num_sent])
+    pos_pair_dist = tf.reshape(tf.boolean_mask(sent_sent_dist, sent_sent_mask), [-1, sent_im_ratio])
+    pos_pair_dist = tf.reduce_max(pos_pair_dist, axis=1, keep_dims=True)
+    neg_pair_dist = tf.reshape(tf.boolean_mask(sent_sent_dist, ~sent_sent_mask), [num_sent, -1])
+    sent_only_loss = tf.clip_by_value(margin + pos_pair_dist - neg_pair_dist, 0, 1e6)
+    sent_only_loss = tf.reduce_mean(tf.nn.top_k(sent_only_loss, k=4)[0])
 
-def compute_loss(corr_X, neg_X):
-	corr_X_emb, neg_X_emb = network(corr_X, neg_X)
-	loss = pairwise_loss(corr_X_emb, neg_X_emb)
-	return loss
+    loss = im_loss * im_loss_factor + sent_loss + sent_only_loss * sent_only_loss_factor
+    
+    return loss
